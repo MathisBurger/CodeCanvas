75809a15d050130faf878932c145f68f5aa575af
Mathis Burger <handymathis@gmail.com>
2024-09-08 15:55:23 +0200

    [FEAT] Added initial app structure

---
+++ authy/src/config.rs
@@ -0,0 +1,21 @@
+use std::collections::HashMap;
+use figment::{Error, Figment};
+use figment::providers::{Env, Format, Toml};
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct AppConfig {
+    pub database_url: String,
+    pub service_locations: HashMap<String, String>
+}
+
+impl AppConfig {
+
+    pub fn parse() -> Result<Self, Error> {
+        let config: AppConfig = Figment::new()
+            .merge(Toml::file("config.toml"))
+            .merge(Env::raw())
+            .extract()?;
+        Ok(config)
+    }
+}
--- authy/src/main.rs
+++ authy/src/main.rs
@@ -1,3 +1,43 @@
-fn main() {
-    println!("Hello, world!");
+mod config;
+
+extern crate pretty_env_logger;
+#[macro_use]
+extern crate log;
+
+use actix_web::{App, HttpServer};
+use actix_web::web::Data;
+use crate::config::AppConfig;
+
+#[derive(Clone)]
+pub struct State {
+    pub config: AppConfig,
+}
+
+#[actix_web::main]
+async fn main() -> Result<(), std::io::Error> {
+    let log_level = std::env::var("RUST_LOG").unwrap_or_else(|_e| "info".to_string());
+
+    std::env::set_var("RUST_LOG", log_level);
+    pretty_env_logger::init();
+    info!(target: "startup", "");
+
+    let config = match AppConfig::parse() {
+        Ok(config) => config,
+        Err(err) => {
+            println!("Could not read config");
+            println!("Error: {:?}", err);
+            std::process::exit(2);
+        }
+    };
+    let state = State {
+        config: config.clone(),
+    };
+    HttpServer::new(move || {
+        App::new()
+            .app_data(Data::new(state.clone()))
+    })
+        .bind("0.0.0.0:3000")
+        .expect("Already in use")
+        .run()
+        .await
 }
