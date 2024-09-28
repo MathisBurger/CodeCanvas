2f9c0fef9b84a648cafdf0b2f4f412f61db2370b
Mathis Burger <handymathis@gmail.com>
2024-09-28 16:42:24 +0200

    [FEAT] Added initial functionality to create assignments and to display them

---
+++ web/components/assignments/CreateAssignmentModal.tsx
@@ -0,0 +1,14 @@
+import { Group } from "@/service/types/tasky"
+
+
+interface CreateAssignmentModalProps {
+    group: Group|null
+}
+
+const CreateAssignmentModal = ({group}: CreateAssignmentModalProps) => {
+
+
+    return (
+        <Modal
+    )
+}
