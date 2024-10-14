b84a61f74971d2486f1f3151fd37aa92c1cae5e6
Mathis Burger <handymathis@gmail.com>
2024-10-14 22:07:29 +0200

    [FIX] Fixed solution button hidden on too large main screens

--- web/app/my-groups/page.tsx
+++ web/app/my-groups/page.tsx
@@ -6,16 +6,22 @@ import useApiServiceClient from "@/hooks/useApiServiceClient";
 import useClientQuery from "@/hooks/useClientQuery";
 import {useState} from "react";
 import CreateGroupModal from "@/components/group/CreateGroupModal";
+import useCurrentUser from "@/hooks/useCurrentUser";
+import { isGranted } from "@/service/auth";
+import { UserRoles } from "@/service/types/usernator";
 
 const GroupsPage = () => {
   const api = useApiServiceClient();
   const [groups] = useClientQuery<GroupsResponse>(() => api.getMyGroups());
   const [createModalOpen, setCreateModalOpen] = useState(false);
+  const {user} = useCurrentUser();
 
   return (
     <Container fluid>
       <Title>My Groups</Title>
-        <Button onClick={() => setCreateModalOpen(true)}>Create group</Button>
+        {isGranted(user, [UserRoles.Tutor, UserRoles.Admin]) && (
+            <Button onClick={() => setCreateModalOpen(true)}>Create group</Button>
+        )}
       <GroupsDisplayComponent groups={groups?.groups ?? []} page="my-groups" />
         {createModalOpen && (
             <CreateGroupModal onClose={() => setCreateModalOpen(false)} />
