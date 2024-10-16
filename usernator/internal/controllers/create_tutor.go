cbdf4062c7e9b5617089d99c8bcabdf3a546dfec
Mathis Burger <handymathis@gmail.com>
2024-10-16 10:23:00 +0200

    fix: Fixed uncomplete removal of rabbitmq

--- usernator/internal/controllers/create_tutor.go
+++ usernator/internal/controllers/create_tutor.go
@@ -1,12 +1,8 @@
 package controllers
 
 import (
-	"context"
-	"encoding/json"
 	"github.com/gofiber/fiber/v2"
-	"github.com/rabbitmq/amqp091-go"
 	"golang.org/x/crypto/bcrypt"
-	"time"
 	"usernator/internal/models"
 	"usernator/internal/shared"
 	"usernator/pkg"
@@ -45,18 +41,5 @@ func CreateTutor(ctx *fiber.Ctx) error {
 	user.ResetToken = nil
 	user.Roles = []string{"ROLE_TUTOR"}
 	shared.Database.Create(&user)
-	rmqctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-	data, err := json.Marshal(user)
-	if err != nil {
-		return fiber.NewError(fiber.StatusInternalServerError, err.Error())
-	}
-	err = shared.RabbitMQ.PublishWithContext(rmqctx, "global_create_user", "", false, false, amqp091.Publishing{
-		ContentType: "application/json",
-		Body:        data,
-	})
-	if err != nil {
-		return fiber.NewError(fiber.StatusInternalServerError, err.Error())
-	}
 	return ctx.JSON(user)
 }
