c56a598964b6c47e8ff79d766bce4d55f4d845a9
Mathis Burger <handymathis@gmail.com>
2024-10-17 16:17:15 +0200

    fix: Fixed test implementations

--- tasky/src/lib.rs
+++ tasky/src/lib.rs
@@ -1,6 +1,8 @@
 use crate::api::usernator_api_client::UsernatorApiClient;
+use crate::grpc::MyTaskyApi;
 use crate::models::database::Database;
 use crate::util::config::AppConfig;
+use log::info;
 use tonic::transport::Channel;
 
 #[derive(Clone)]
@@ -30,4 +32,42 @@ pub mod api {
 
 pub mod tasky_grpc {
     tonic::include_proto!("tasky_grpc");
+}
+
+pub async fn get_states() -> (AppState, MyTaskyApi) {
+    let log_level = std::env::var("RUST_LOG").unwrap_or_else(|_e| "info".to_string());
+
+    std::env::set_var("RUST_LOG", log_level);
+    pretty_env_logger::init();
+    info!(target: "startup", "");
+
+    let config = match AppConfig::parse() {
+        Ok(config) => config,
+        Err(err) => {
+            println!("Could not read config");
+            println!("Error: {:?}", err);
+            std::process::exit(2);
+        }
+    };
+    let db = Database::new(config.clone());
+    let user_api_uri = config.clone().usernator_grpc;
+    info!(target: "startup", "{}", format!("Connecting to usernator: {}", user_api_uri));
+    let usernator = UsernatorApiClient::connect(user_api_uri)
+        .await
+        .expect("Cannot create tonic client");
+
+    let mongodb = mongo::connect(config.clone()).await;
+
+    let state = AppState {
+        config: config.clone(),
+        db,
+        mongodb,
+        user_api: usernator,
+    };
+
+    let tasky_api = MyTaskyApi {
+        app_state: state.clone(),
+    };
+
+    (state, tasky_api)
 }
--- tasky/src/main.rs
+++ tasky/src/main.rs
@@ -1,56 +1,19 @@
+use actix_web::middleware;
 use actix_web::web::Data;
-use actix_web::{middleware, App, HttpServer};
+use actix_web::App;
+use actix_web::HttpServer;
 use futures::future::join;
-use log::info;
 use std::net::SocketAddr;
-use tasky::api::usernator_api_client::UsernatorApiClient;
 use tasky::auth_middleware::Auth;
-use tasky::grpc::MyTaskyApi;
-use tasky::models::database::Database;
-use tasky::mongo;
 use tasky::routes::init_services;
 use tasky::tasky_grpc::tasky_api_server::TaskyApiServer;
-use tasky::util::config::AppConfig;
-use tasky::AppState;
 use tonic::transport::Server;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let log_level = std::env::var("RUST_LOG").unwrap_or_else(|_e| "info".to_string());
-
-    std::env::set_var("RUST_LOG", log_level);
-    pretty_env_logger::init();
-    info!(target: "startup", "");
-
-    let config = match AppConfig::parse() {
-        Ok(config) => config,
-        Err(err) => {
-            println!("Could not read config");
-            println!("Error: {:?}", err);
-            std::process::exit(2);
-        }
-    };
-    let db = Database::new(config.clone());
-    let user_api_uri = config.clone().usernator_grpc;
-    info!(target: "startup", "{}", format!("Connecting to usernator: {}", user_api_uri));
-    let usernator = UsernatorApiClient::connect(user_api_uri)
-        .await
-        .expect("Cannot create tonic client");
-
-    let mongodb = mongo::connect(config.clone()).await;
-
-    let state = AppState {
-        config: config.clone(),
-        db,
-        mongodb,
-        user_api: usernator,
-    };
-
-    let tasky_api = MyTaskyApi {
-        app_state: state.clone(),
-    };
-
     let grpc_sock_addr: SocketAddr = "0.0.0.0:3001".parse().unwrap();
+
+    let (state, tasky_api) = tasky::get_states().await;
 
     let grpc = async move {
         tokio::task::spawn(
@@ -60,7 +23,6 @@ async fn main() -> std::io::Result<()> {
         )
         .await
     };
-
     let actix = HttpServer::new(move || {
         App::new()
             .wrap(middleware::Logger::default())
