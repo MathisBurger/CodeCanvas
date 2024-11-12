d4a0ac38fcbd5e46dac39111e2368feb6c1e1383
Mathis Burger <handymathis@gmail.com>
2024-11-12 10:20:48 +0100

    feat: Implemented near search for spotlight search

--- web/hooks/spotlight/useActionsFactory.ts
+++ web/hooks/spotlight/useActionsFactory.ts
@@ -13,6 +13,16 @@ const useActionsFactory = (search: string): (
   const stage2Actions = useStage2Actions();
   const stage3Actions = useStage3Actions(search);
 
+  const markedStage3Actions = useMemo<SpotlightActionGroupData[]>(() => {
+    return stage3Actions.map((group) => {
+      group.actions = group.actions.map((action) => {
+        action.id = action.id + "-stage3";
+        return action;
+      });
+      return group;
+    });
+  }, [stage3Actions])
+
   const filterOut = (stage3: SpotlightActionData[], stage2: SpotlightActionData[]) => {
     const stage3Ids = stage3.map((element) => element.id);
     return stage2.filter((element) => stage3Ids.indexOf(element.id) === -1);
@@ -21,17 +31,17 @@ const useActionsFactory = (search: string): (
   /**
    * Merges spotlight stage2 and stage3 groups together
    */
-  const mergedStageActions = useMemo<SpotlightActionGroupData[]>(() => {
+  const reducedStage2 = useMemo<SpotlightActionGroupData[]>(() => {
     return stage2Actions.map((group, i) => {
       const actionsStage3 = stage3Actions[i]?.actions ?? [];
-      group.actions = filterOut(actionsStage3, group.actions).concat(actionsStage3);
+      group.actions = filterOut(actionsStage3, group.actions);
       return group;
     });
   }, [stage2Actions, stage3Actions]);
 
   return useMemo<
       (SpotlightActionGroupData | SpotlightActionData)[]
-  >(() => [...staticRoutes, ...staticGeneral, ...mergedStageActions], [mergedStageActions, staticGeneral, staticRoutes]);
+  >(() => [...staticRoutes, ...staticGeneral, ...reducedStage2, ...markedStage3Actions], [reducedStage2, markedStage3Actions, staticGeneral, staticRoutes]);
 };
 
 export default useActionsFactory;
