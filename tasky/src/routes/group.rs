d5a691aa9bd884ab1516f496f72bbd4dab54b4e3
Mathis Burger <handymathis@gmail.com>
2024-11-14 20:25:42 +0100

    chore: Fixed clippy linting

--- tasky/src/routes/group.rs
+++ tasky/src/routes/group.rs
@@ -218,7 +218,8 @@ pub async fn remove_user(
         .members
         .iter()
         .filter(|m| m.is_some() && m.unwrap() != path_data.1)
-        .copied();
+        .copied()
+        .collect();
     GroupRepository::update_group(group, conn);
     Ok(HttpResponse::Ok().finish())
 }
