34de0b6d352d428c1958dc2f1573431f827a67af
Mathis Burger <handymathis@gmail.com>
2024-11-18 19:00:32 +0100

    feat: Added verification REST endpoints

--- tasky/src/routes/mod.rs
+++ tasky/src/routes/mod.rs
@@ -32,6 +32,8 @@ pub fn init_services(cfg: &mut web::ServiceConfig) {
         .service(group::remove_user)
         .service(group::leave_group)
         .service(group::delete_group)
+        .service(group::verify_group)
+        .service(group::unverify_group)
         .service(group_join_request::create_join_request)
         .service(group_join_request::get_join_requests)
         .service(group_join_request::approve_join_request)
