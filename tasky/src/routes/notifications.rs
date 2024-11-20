172e49cc2908f109c7730610feec4e669e124c28
Mathis Burger <handymathis@gmail.com>
2024-11-20 14:22:16 +0100

    feat: Added endpoint for creating group notifications

--- tasky/src/routes/notifications.rs
+++ tasky/src/routes/notifications.rs
@@ -1,6 +1,13 @@
-use actix_web::{delete, get, web, HttpResponse};
+use actix_web::{delete, get, post, web, HttpResponse};
+use serde::Deserialize;
 
-use crate::{auth_middleware::UserData, models::notification::NotificationRepository, AppState};
+use crate::{
+    auth_middleware::UserData,
+    error::ApiError,
+    models::{group::GroupRepository, notification::NotificationRepository},
+    security::{IsGranted, SecurityAction},
+    AppState,
+};
 
 /// Gets all notifications
 #[get("/notifications")]
@@ -41,4 +48,38 @@ pub async fn remove_user_from_all_notifications(
     let db = &mut data.db.db.get().unwrap();
     NotificationRepository::remove_user_from_all_notification(user_data.user_id, db);
     HttpResponse::Ok().finish()
+}
+
+#[derive(Deserialize)]
+struct CreateNotificationRequest {
+    pub title: String,
+    pub content: String,
+}
+
+/// Endpoint to create group notification
+#[post("/groups/{id}/notifications")]
+pub async fn create_group_notification(
+    data: web::Data<AppState>,
+    user: web::ReqData<UserData>,
+    body: web::Json<CreateNotificationRequest>,
+    path: web::Path<(i32,)>,
+) -> Result<HttpResponse, ApiError> {
+    let conn = &mut data.db.db.get().unwrap();
+
+    let mut group =
+        GroupRepository::get_by_id(path.into_inner().0, conn).ok_or(ApiError::BadRequest {
+            message: "No access to group".to_string(),
+        })?;
+    if !group.is_granted(SecurityAction::Update, &user) {
+        return Err(ApiError::Unauthorized {
+            message: "Not authorized for action".to_string(),
+        });
+    }
+    NotificationRepository::create_notification_for_group(
+        body.title.clone(),
+        body.content.clone(),
+        group.id,
+        conn,
+    );
+    Ok(HttpResponse::Ok().finish())
 }
